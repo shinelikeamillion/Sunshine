0e84c7f40c60df9ba3d6b8458f64ed1162b18af7
Sean <geeks.liu@gmail.com>
2016-11-18 17:42:19 +0800

    Coding Task on Accessibility

--- app/src/main/java/com/udacity/sunshine/fragment/DetailFragment.java
+++ app/src/main/java/com/udacity/sunshine/fragment/DetailFragment.java
@@ -213,6 +213,9 @@ public class DetailFragment extends Fragment implements LoaderCallbacks<Cursor> 
             String description = data.getString(COL_WEATHER_DESC);
             mDescriptionView.setText(description);
 
+            // For accessibility, add a content description to the icon field
+            mIconView.setContentDescription(description);
+
             // Read high temperature from cursor and update view
             boolean isMetric = Utility.isMetric(getActivity());
 
